cf5688f2caf842f5a458efdda7346b7455140d37
tenid <suj710101@gmail.com>
2021-04-16 10:35:18 +0900

    Add function: show author from index.html, question_deatil.html

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-
+from django.contrib.auth.decorators import login_required
 
 app_name='main'
 urlpatterns = [
     path('', views.index.as_view(), name='index'),
     path('<int:pk>/', views.QuestionDetail.as_view(), name='detail'),
-    path('answer/create/<int:question_id>/', views.answer_create,name='answer_create'),
-    path('question/create/',views.QuestionCreate.as_view(), name='question_create'),
+    path('answer/create/<int:question_id>/', login_required(views.answer_create, login_url='common:login'),name='answer_create'),
+    path('question/create/',login_required(views.QuestionCreate.as_view(), login_url='common:login'), name='question_create'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,28 +1,10 @@
 
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from django.core.paginator import Paginator
+
 from .forms import *
 from django.views.generic import ListView, DetailView, View
 
-# Create your views here.
-
-
-# def index(request):
-#     question_list = Question.objects.order_by('-create_date')
-#     context = {
-#         'page_title': '질문과 답변',
-#         'question_list': question_list
-#     }
-#     return render(request, 'main/index.html', context)
-#
-# def detail(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     context = {
-#         'page_title': '질문 보기',
-#         'question': question
-#     }
-#     return render(request, 'main/question_detail.html', context)
 
 class index(ListView):
     model = Question
@@ -49,6 +31,7 @@ class QuestionDetail(DetailView):
         return context
 
 
+
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     if request.method == "POST":
@@ -57,6 +40,7 @@ def answer_create(request, question_id):
             answer = form.save(commit=False)
             answer.create_date = timezone.now()
             answer.question = question
+            answer.author = request.user
             answer.save()
             return redirect('main:detail', pk=question.id)
     else:
@@ -71,6 +55,7 @@ class QuestionCreate(View):
         if form.is_valid():
             question = form.save(commit=False)
             question.create_date = timezone.now()
+            question.author = request.user
             question.save()
             return redirect('main:index')
         else:
