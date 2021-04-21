9e1781d6f7e1d717890776ffe0d83fa3a7a8a922
tenid <suj710101@gmail.com>
2021-04-21 11:11:39 +0900

    Add function: Post sorting Modify docs: Readme.md

--- QnA/views/base_views.py
+++ QnA/views/base_views.py
@@ -3,8 +3,8 @@ from django.views.generic import DetailView, ListView
 from Post.models import Category
 from QnA.models import Question
 
-# Django model ORM로 Where절에 or문을 사용할 경우
-from django.db.models import Q
+# Django model ORM로 Where절에 or문을 사용할 경우, 총 갯수 파악
+from django.db.models import Q, Count
 
 
 class Index(ListView):
@@ -14,8 +14,9 @@ class Index(ListView):
 
     # get 요청으로 보낸 데이터 처리
     def get(self, request, *args, **kwargs):
-        self.kw = request.GET.get('kw')   # 검색 텍스트
-        self.page = request.GET.get('page')  # 페이지 번호
+        self.kw = request.GET.get('kw', '')   # 검색 텍스트
+        self.page = request.GET.get('page','1')  # 페이지 번호
+        self.so = request.GET.get('so', 'recent')
         return super(Index, self).get(request, *args, **kwargs)
 
     # 추가로 필요한 데이터 처리
@@ -24,11 +25,21 @@ class Index(ListView):
         category_list = Category.objects.order_by('-created_at').all()
         context['category_list'] = category_list
         context['page_title'] = '질문과 답변'
+        context['so'] = self.so
         return context
 
     # 쿼리 명령 처리
     def get_queryset(self):
-        question_list = self.model.objects.order_by('-create_date')
+        # 추천순 정렬
+        if self.so == 'recommend':
+            question_list = self.model.objects.annotate(num_voter=Count('voter')).order_by('-num_voter', '-create_date')
+        # 인기순 정렬(답글 수)
+        elif self.so == 'popular':
+            question_list = self.model.objects.annotate(num_answer=Count('answer')).order_by('-num_answer','-create_date')
+        # 최근순 정렬
+        else:
+            question_list = self.model.objects.order_by('-create_date')
+        # 검색어 필터
         if self.kw:
             question_list = question_list.filter(
                 Q(subject__icontains=self.kw) |  # 제목검색
--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # Django-Myblog
 <img src="docs/images/QnA_index.png" align="middle"/>
 
-## Version
-- Django 3.2  
-- Python 3.6.7
+## Version && Develop environment
+- OS: Mac OS(Big Sur)
+- FrameWork: Django(3.2), BootStrap
+- Language: Python(3.6.7), HTML, JavaScript
+- DB: Mysql(8.0.23)
 
 ## Function
 - 회원가입, 로그인, 로그아웃
@@ -12,6 +14,10 @@
 - 게시판 내 검색
 - 마크다운 게시물 작성
 - 게시글 추천, 댓글 
+- 게시글 정렬순서(최신순,추천순,인기순)
+- 소셜 로그인, 게시판 조회수, 파일 업로드, 마크다운 Editor, 답변 페이징 정렬 (개발 예정)
 
 ## Reference
-[점프 투 장고: 박은용님](https://wikidocs.net/book/4223)
+- [점프 투 장고: 박은용님](https://wikidocs.net/book/4223)
+- [Django와 MySQL 연결하기](https://velog.io/@devmin/Django-MySQL-Connect)  
+    - [Mysqlcilent 설치 시 에러(mysql_config not found) 해결방법](https://elfinlas.github.io/2019/01/23/pip-mysql-error/)
